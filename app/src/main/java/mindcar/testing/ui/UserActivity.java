6db040840d90197fad0457f9bb4ac761bec52618
Johan Laptop <johan.johansson.swe@gmail.com>
2016-04-27 12:13:39 +0200

    Patterns work again

--- app/src/main/java/mindcar/testing/ui/UserActivity.java
+++ app/src/main/java/mindcar/testing/ui/UserActivity.java
@@ -1,6 +1,5 @@
 package mindcar.testing.ui;
 
-
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,10 +8,8 @@ import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-
 import com.neurosky.thinkgear.TGDevice;
 import com.neurosky.thinkgear.TGEegPower;
-
 import mindcar.testing.R;
 import mindcar.testing.objects.Command;
 import mindcar.testing.objects.ComparePatterns;
@@ -53,6 +50,7 @@ public class UserActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(UserActivity.this, SavePatterns.class));
+                tgDevice.stop();
             }
         });
         }
