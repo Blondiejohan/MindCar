ad389f6abaf039e5d95348f3d61510252e03f8b6
sanjuska <colak.sanja@yahoo.com>
2016-05-01 20:31:20 +0200

    GUI Intro StartActivity Register Sounds

--- app/src/main/java/mindcar/testing/ui/StartActivity.java
+++ app/src/main/java/mindcar/testing/ui/StartActivity.java
@@ -2,8 +2,10 @@ package mindcar.testing.ui;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,6 +21,7 @@ public class StartActivity extends Activity implements View.OnClickListener {
     EditText ET_USER_NAME, ET_PASSWORD;
     TextView devView;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +32,7 @@ public class StartActivity extends Activity implements View.OnClickListener {
         bLogin = (Button) findViewById(R.id.bLogin);
         bSINGUP = (Button) findViewById(R.id.bSIGNUP);
         devView=(TextView) findViewById(R.id.devView);
+
 
         bLogin.setOnClickListener(this);
         bSINGUP.setOnClickListener(this);
@@ -44,12 +48,18 @@ public class StartActivity extends Activity implements View.OnClickListener {
                 if (databaseAccess.checkUser(ET_USER_NAME.getText().toString(), ET_PASSWORD.getText().toString())) {
                     Toast.makeText(getApplicationContext(), "Login successful", Toast.LENGTH_SHORT).show();
                     //databaseAccess.close();
+                    MediaPlayer mp = MediaPlayer.create(this, R.raw.yes);
+                    mp.start();
                     startActivity(new Intent(this, BluetoothActivity.class));
                     //startActivity(new Intent(this, Connection.class));
                 }
-                else
+                else {
+
                     Toast.makeText(getApplicationContext(), "Wrong username or password", Toast.LENGTH_LONG).show();
-                break;
+                    MediaPlayer mp2 = MediaPlayer.create(this, R.raw.no);
+                    mp2.start();
+                    break;
+                }
 
             case R.id.bSIGNUP:
                 startActivity(new Intent(this, RegistrationActivity.class));
