cb3aa0a9054507a8a3201548fdb4dc8b34422878
nmsasopoulos <nmsasopoulos@gmail.com>
2016-05-13 11:46:47 +0200

    upd

---
+++ app/src/main/java/mindcar/testing/objects/EegBlink.java
@@ -0,0 +1,48 @@
+package mindcar.testing.objects;
+
+import com.neurosky.thinkgear.TGDevice;
+
+/**
+ * Created by Darkthronen on 13/05/2016.
+ */
+public class EegBlink {
+
+    private int blink;
+    private int attention;
+
+    public EegBlink(int blink, int attention) {
+        this.blink = blink;
+        this.attention = attention;
+    }
+
+    public int getBlink() {
+        return blink;
+    }
+
+    public void setBlink(int blink) {
+        this.blink = blink;
+    }
+
+    public int getAttention() {
+        return attention;
+    }
+
+    public void setAttention(int attention) {
+        this.attention = attention;
+    }
+
+    public boolean test1(){
+        if (blink == 2 && attention>10){
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public boolean test2() {
+        if (blink == 3 && attention > 10){
+            return true;}
+        else
+            return false;
+    }
+}
